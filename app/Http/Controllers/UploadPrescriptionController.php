c8857a924e003d07dccb26d99121834be4593a13
danielawaldeck95 <danielawaldeck95@gmail.com>
2023-04-17 12:29:45 -0300

    feat: create upload controller and form request

---
+++ app/Http/Controllers/UploadPrescriptionController.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Enums\SubmissionStatuses;
+use App\Http\Requests\UploadPrescriptionRequest;
+use App\Models\Submission;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Storage;
+
+class UploadPrescriptionController extends Controller
+{
+    public function __invoke(UploadPrescriptionRequest $request, Submission $submission): JsonResponse
+    {
+        $extension = $request->file('prescription')->extension();
+        $mimeType = $request->file('prescription')->getMimeType();
+
+        $path = Storage::putFileAs(
+            config('filesystems.disks.do_spaces.folder'),
+            $request->file('prescription'),
+            $submission->id . '.' . $extension
+        );
+
+        $submission->prescription()->create([
+            'name' => $submission->id . '.' . $extension,
+            'path' => $path,
+            'mime_type' => $mimeType,
+            'size' => $request->file('prescription')->getSize()
+        ]);
+
+        $submission->status = SubmissionStatuses::Done;
+        $submission->save();
+
+        return response()->json($submission);
+    }
+}
---
+++ app/Http/Requests/UploadPrescriptionRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Http\Requests;
+
+use App\Enums\SubmissionStatuses;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UploadPrescriptionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        $submissionIsPending = $this->submission->status == SubmissionStatuses::Pending;
+        $doctorIsAssignedToThisSubmission = $this->submission->doctor_id == $this->user()->id;
+        return ! $submissionIsPending && $doctorIsAssignedToThisSubmission;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'prescription' => ['required', 'mimes:txt']
+        ];
+    }
+}
