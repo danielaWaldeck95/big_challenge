78d728eec83d77cd3f23538a042dcf1cf2e808a3
danielawaldeck95 <danielawaldeck95@gmail.com>
2023-04-19 11:18:53 -0300

    feat: test filter by status

--- tests/Feature/GetPatientSubmissionsTest.php
+++ tests/Feature/GetPatientSubmissionsTest.php
@@ -1,29 +1,41 @@
 <?php
 
+use App\Enums\SubmissionStatuses;
 use App\Models\Submission;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
-const totalSubmissionsNumber = 6;
-it('cannot get submissions if it is not logged in', function () {
-    $response = $this->getJson(route('submissions.index'), []);
+const totalSubmissionsNumber = 3;
+
+beforeEach(function () {
+    $this->patient = User::newFactory()->patient()->patientInformation()->create();
+    $this->doctor = User::newFactory()->doctor()->create();
+    $this->pendingSubmission = Submission::newFactory()->create(['patient_id' => $this->patient->id]);
+    $this->inProgressSubmission = Submission::newFactory()
+    ->count(totalSubmissionsNumber)->create([
+        'patient_id' => $this->patient->id,
+        'doctor_id' => $this->doctor->id,
+        'status' => SubmissionStatuses::InProgress
+    ]);
+    $this->otherPatientSubmission = Submission::newFactory()->create();
+});
+
+it('throws an error when trying to get submissions as a guest', function () {
+    $response = $this->getJson(route('submissions.index'));
     $response->assertUnauthorized();
 });
 
 test('get my submissions successfully as a patient', function () {
-    $user = User::newFactory()->patient()->patientInformation()->create();
-    $this->actingAs($user);
+    $response = $this->actingAs($this->patient)->getJson(route('submissions.index'));
+    $response->assertSuccessful()->assertJsonCount(4, 'data');
+});
 
-    Submission::newFactory()
-    ->count(totalSubmissionsNumber)
-    ->state(new Sequence(
-        ['patient_id' => $user->id],
-        ['patient_id' => User::newFactory()->patient()->patientInformation()->create()],
-    ))
-    ->create();
+it('filters patient submissions by status', function () {
+    $response = $this->actingAs($this->patient)->getJson(route('submissions.index', ['status' => 'pending']));
+    $response->assertSuccessful()->assertJsonCount(1, 'data');
 
-    $response = $this->getJson(route('submissions.index'));
-    $response->assertSuccessful()->assertJsonCount(totalSubmissionsNumber / 2, 'data');
+    $response = $this->actingAs($this->patient)->getJson(route('submissions.index', ['status' => 'in progress']));
+    $response->assertSuccessful()->assertJsonCount(3, 'data');
 });
