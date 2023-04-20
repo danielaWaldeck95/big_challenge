60e9d4b6d4a20f220a9772aa2591e470e43b9bfa
danielawaldeck95 <danielawaldeck95@gmail.com>
2023-04-20 12:05:43 -0300

    feat: rename controller that retrieves submissions for user

--- app/Http/Controllers/GetUserSubmissionsController.php
+++ app/Http/Controllers/GetUserSubmissionsController.php
@@ -7,7 +7,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class GetSubmissionsController
+class GetUserSubmissionsController
 {
     /**
      * Handle the incoming request.
